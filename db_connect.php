9d7f09cc88ec90e297129b4a1fb0b87850942a58
go326 <kiku@localhost.localdomain>
2020-12-17 16:22:00 +0900

    試しです．

---
+++ db_connect.php
@@ -0,0 +1,11 @@
+<?php
+
+
+    $dsn = 'mysql:dbname=admin;host=localhost';
+    $user = 'admin';
+    $password = 'software_ex_g3';
+
+    $pdo = new PDO($dsn, $user, $password);
+    echo ('接続に成功');
+
+?>
