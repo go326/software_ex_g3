6a4b6d3bcdc44fe628f6e9694e493426e4f3bc01
220299v <220299v@users.noreply.github.com>
2020-12-23 21:55:23 +0900

    12/23　select文テスト

--- seisou/s_clean_management.php
+++ seisou/s_clean_management.php
@@ -1,5 +1,18 @@
 <?php
-
+    
     include '../db_connect.php';
+    
+function SCleanManagemantP(){
+    $stmt = $pdo -> query("SELECT * FROM room");
+    while ($row = $stmt -> fetch()){
+        $room_number = $row["room_number"];
+        $room_clean = $row["room_clean"];
+        echo "($room_number) $room_clean"
+    }
+
+}
+
+SCleanManagemantP();
+
 
 ?>
