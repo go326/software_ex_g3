99be845504d22f2c66af2aa85e5b7cd766450650
220299v <220299v@users.noreply.github.com>
2020-12-24 14:40:36 +0900

    12/24 変数をglobalにしました。

--- seisou/s_clean_management.php
+++ seisou/s_clean_management.php
@@ -3,11 +3,12 @@
     include '../db_connect.php';
     
 function SCleanManagemantP(){
+    global $pdo,$room_number,$room_clean;
     $stmt = $pdo -> query("SELECT * FROM room");
     while ($row = $stmt -> fetch()){
         $room_number = $row["room_number"];
         $room_clean = $row["room_clean"];
-        echo "($room_number) $room_clean"
+        echo ('($room_number) $room_clean');
     }
 
 }
