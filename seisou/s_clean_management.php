d755128df1d1c47bbb2ccb2fcc659a6197b44fa5
220299v <220299v@users.noreply.github.com>
2020-12-25 15:04:28 +0900

    戻しました

--- seisou/s_clean_management.php
+++ seisou/s_clean_management.php
@@ -6,7 +6,7 @@ function SCleanManagemantP(){
     global $pdo,$room_number,$room_clean;
     $stmt = $pdo -> query("SELECT * FROM room");
     //fetch
-    while ($row = $stmt -> fetch(PDO::FETCH_COLUMN)){
+    while ($row = $stmt -> fetch()){
         $room_number = $row["room_number"];
         $room_clean = $row["room_clean"];
         echo ($room_number.",".$room_clean."<br>");
