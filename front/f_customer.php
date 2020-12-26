d85de392e61bb00f30330c401f057de3aa88964b
go326 <kiku@localhost.localdomain>
2020-12-27 00:24:31 +0900

    customer.phpを変更しました．

--- front/f_customer.php
+++ front/f_customer.php
@@ -5,10 +5,21 @@ include("../db_connect.php");
 
 global $pdo;
 
-$sql = "SELECT * FROM customer ";
-
-$smt = $pdo->query($sql);
-
-$data = $smt->fetch();
-
-var_dump($data);
+//insert か select か判定
+if(isset($_POST['reservation'])){
+    //insert
+    var_dump($_POST);
+    foreach($_POST as $value){
+        $sql = "INSERT into customer values(' . implode(',', array_fill(0, count($value), '?')) . ')";
+        echo $sql;
+        //$smt = $pdo ->prepare($sql);
+        //$smt ->sxecute($parms);
+    }
+} else {
+    //select
+    $sql = "SELECT * FROM customer ";
+    echo $sql;
+    //$smt = $pdo->query($sql);
+    //$data = $smt->fetch();
+    //var_dump($data);
+}
