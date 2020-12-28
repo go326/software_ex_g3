f9b494373f4f4446acbf306083390dc38189c375
go326 <220321r@ugs.kochi-tech.ac.jp>
2020-12-28 20:51:17 +0900

    f_information.phpを編集しました。

--- front/f_information/f_information.php
+++ front/f_information/f_information.php
@@ -7,9 +7,10 @@ include("../../db_connect.php");
 $sql = "SELECT * FROM customer ";
 
 $smt = $pdo->query($sql);
-$data = $smt->fetch();
+$data = $smt->fetch(PDO::FETCH_NUM);
 var_dump($data);
 
+$ref = 
 ?>
 
 <html>
@@ -30,76 +31,76 @@ var_dump($data);
         <dl id="id">
             <dt> 予約ID </dt?>
             <dd>
-                <?php echo $data; ?>
+                <?php echo $data[0]; ?>
             </dd>
         </dl>
         <dl id="day">
             <dt> 予約日 </dt?>
             <dd>
-                <?php echo $data; ?>
+                <?php echo $data[2]; ?>
             </dd>
             <dt> 宿泊日 </dt>
             <dd>
-                <?php echo $data; ?>
+                <?php echo $data[1]; ?>
             </dd>
         </dl>
         <dl id="customer">
             <dt> 氏名 </dt?>
             <dd>
-                <?php echo $data; ?>
+                <?php echo $data[4]; ?>
             </dd>
             <dt> 住所 </dt>
             <dd>
-                <?php echo $data; ?>
+                <?php echo $data[5]; ?>
             </dd>
             <dt> 電話番号 </dt>
             <dd>
-                <?php echo $data; ?>
+                <?php echo $data[6]; ?>
             </dd>
         </dl>
         <dl id="counter">
             <dt> 人数 </dt?>
             <dd>
-                <?php echo $data; ?>
+                <?php echo $data[7]; ?>
             </dd>
             <dt> 人数 </dt>
             <dd>
-                <?php echo $data; ?>
+                <?php echo $data[8]; ?>
             </dd>
         </dl>
         <dl id="plan">
             <dt> プラン </dt?>
             <dd>
-                <?php echo $data; ?>
+                <?php echo $data[9]; ?>
             </dd>
             <dt> 夕食 </dt>
             <dd>
-                <?php echo $data; ?>
+                <?php echo $data[10]; ?>
             </dd>
             <dt> 夕食メニュー </dt>
             <dd>
-                <?php echo $data; ?>
+                <?php echo $data[11]; ?>
             </dd>
             <dt> 朝食 </dt>
             <dd>
-                <?php echo $data; ?>
+                <?php echo $data[12]; ?>
             </dd>
             <dt> 朝食メニュー </dt>
             <dd>
-                <?php echo $data; ?>
+                <?php echo $data[13]; ?>
             </dd>
         </dl>
 
         <dl id="room">
             <dt> 部屋番号</dt?>
             <dd>
-                <?php echo $data; ?>
+                <?php echo $data[14] . " " . $data[15] . " " . $data[16]; ?>
             </dd>
         </dl>
         <dl id="remark">
             <dt> 備考</dt?>
             <dd>
-                <?php echo $data; ?>
+                <?php echo $data[18]; ?>
             </dd>
         </dl>
     </div>
