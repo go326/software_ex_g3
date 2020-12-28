c56b429f53ff6cc8f70d86de903400e8c410a2f0
go326 <220321r@ugs.kochi-tech.ac.jp>
2020-12-28 14:42:51 +0900

    maketables.jsをへんしゅうしました．

--- front/maketable.js
+++ front/maketable.js
@@ -9,17 +9,17 @@ const ID_NAME = "maintable";//取得するID
 const LINK_HTML = "f_information/f_information_details.html";
 
 /**Tableの生成関数 */
-function makeTable(tabledata){
+function makeTable(tabledata) {
   //table要素の生成
   var table = document.createElement('table');
   var count = 0;
-  for(var i = 0; i < 4; i++){
+  for (var i = 0; i < 4; i++) {
     //tr要素の生成
     var tr = document.createElement('tr');
     //LINE_BREAK分のdd(部屋)を並べると改行
-    for(var j = 0; j < LINE_BREAK; j++){
+    for (var j = 0; j < LINE_BREAK; j++) {
       //1フロアの部屋数分を作成するとbreak
-      if(count === NUM_OF_ROOMS){
+      if (count === NUM_OF_ROOMS) {
         break;
       }
       //tdの生成
@@ -28,7 +28,7 @@ function makeTable(tabledata){
       var a = document.createElement('a');
       //href属性追加～tdへaタグを追加(?以降がパラメータ)
       //phpから配列を受け取るように書き換える
-      a.setAttribute("href", LINK_HTML+"?"+tabledata[count]);
+      a.setAttribute("href", LINK_HTML + "?" + tabledata[count]);
       a.textContent = tabledata[count];
       td.appendChild(a);
       //trへtdを追加
@@ -41,7 +41,8 @@ function makeTable(tabledata){
   document.getElementById(ID_NAME).appendChild(table);
 }
 //HTMLからの呼び出し
-function call_makeTable(){
+function call_makeTable() {
+  print(room);
   makeTable(data201_235);
   makeTable(data301_335);
   makeTable(data401_435);
@@ -50,25 +51,25 @@ function call_makeTable(){
 
 //部屋番号の配列
 var data201_235 = [201, 202, 203, 205, 206, 207, 208, 210,
-                   211, 212, 213, 215, 216, 217, 218, 220,
-                   221, 222, 223, 225, 226, 227, 228, 230,
-                   231, 232, 233, 235];
+  211, 212, 213, 215, 216, 217, 218, 220,
+  221, 222, 223, 225, 226, 227, 228, 230,
+  231, 232, 233, 235];
 var data301_335 = [301, 302, 303, 305, 306, 307, 308, 310,
-                   311, 312, 313, 315, 316, 317, 318, 320,
-                   321, 322, 323, 325, 326, 327, 328, 330,
-                   331, 332, 333, 335];
+  311, 312, 313, 315, 316, 317, 318, 320,
+  321, 322, 323, 325, 326, 327, 328, 330,
+  331, 332, 333, 335];
 var data401_435 = [401, 402, 403, 405, 406, 407, 408, 410,
-                   411, 412, 413, 415, 416, 417, 418, 420,
-                   421, 422, 423, 425, 426, 427, 428, 430,
-                   431, 432, 433, 435];
+  411, 412, 413, 415, 416, 417, 418, 420,
+  421, 422, 423, 425, 426, 427, 428, 430,
+  431, 432, 433, 435];
 
 //tableの値の取得
-function getTable(){
+function getTable() {
   alert("in");
   var tableData = document.getElementById(ID_NAME);
-  var rowlen = tableData.rows.length; 
-  for (var i = 0; i < rowlen; i++){
-    for(var j = 0; j < tableData.rows[i].cells.length; j++){
+  var rowlen = tableData.rows.length;
+  for (var i = 0; i < rowlen; i++) {
+    for (var j = 0; j < tableData.rows[i].cells.length; j++) {
       var txtTabledata = tableData.rows[i].cells[j].appendChild[0].value;
     }
   }
