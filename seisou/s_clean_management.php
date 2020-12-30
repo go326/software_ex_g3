c271d6aba69dd5cf424aa592362910c4801ee81f
220299v <220299v@users.noreply.github.com>
2020-12-30 09:43:23 +0900

    12/30

--- seisou/s_clean_management.php
+++ seisou/s_clean_management.php
@@ -39,9 +39,24 @@ function SCleanNumberP(){
 function SCleanEditP($room_number,$room_clean){
     global $pdo;
     echo ($room_number."test".$room_clean."test<br>");
-    $sc_sql = "UPDATE room SET room_clean = ".$room_clean." WHERE room_number = ".$room_number;
-    $stmt = $pdo -> prepare($sc_sql);
-    $stmt -> execute();
+    try{
+        $sc_sql = "UPDATE room SET room_clean = ".$room_clean." WHERE room_number = ".$room_number;
+        $stmt = $pdo -> prepare($sc_sql);
+        $stmt -> execute();
+        echo ("実行に成功しました。<br>");
+        echo ($room_number."号室を");
+        if($room_clean == 0){
+            echo("掃除していない");
+        }else if($room_clean == 1){
+            echo("チェックイン状態");
+        }else if($room_clean == 2){
+            echo("掃除済み");
+        }
+        echo ("に変更しました。<br>");
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit;
+    }
 }
 
 //list($room_number,$room_clean) = SCleanManagemantP();
