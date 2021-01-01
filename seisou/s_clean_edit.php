a01ddb1e30efe2a513f4a35d6027778c6ee5fc97
sugimotowataru <sugimotowataru666@gmail.com>
2021-01-01 19:14:38 +0900

    めっちゃ変えた

--- seisou/s_clean_edit.php
+++ seisou/s_clean_edit.php
@@ -28,14 +28,12 @@
                 <input type = "radio" value="2" name="room_clean">掃除済み<br>
             </form>
             <div class="input#submit_button">
-                <div class="inline">
-                    <!--戻るボタン-->
-                    <form  id = "clean_back" action = "s_clean_management.php">
-                        <input id="submit_button" type="submit" name="submit" value="戻る" onclick="location.href='./s_clean_management.php'"> 
-                    </form>
-                    <!--完了ボタン-->
-                    <input id="submit_button" type="submit" name="submit" value="完了" form = "clean_edit" value="<?php echo $room_number; ?>" name="room_number"></input>
-                </div>
+                <!--戻るボタン-->
+                <form  id = "clean_back" action = "s_clean_management.php">
+                    <input id="submit_button" type="submit" name="submit" value="戻る" onclick="location.href='./s_clean_management.php'"> 
+                </form>
+                <!--完了ボタン-->
+                <input id="submit_button" type="submit" name="submit" value="完了" form = "clean_edit" value="<?php echo $room_number; ?>" name="room_number"></input>
             </div>
         </div>
         <!--フッター-->
