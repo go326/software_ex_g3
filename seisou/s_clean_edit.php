3a9cb1e6435b85c2fe29862d45a75356c7e48221
220299v <220299v@users.noreply.github.com>
2021-01-02 11:38:58 +0900

    1/2 hidden追加

--- seisou/s_clean_edit.php
+++ seisou/s_clean_edit.php
@@ -26,6 +26,7 @@
                 <input type = "radio" value="0" name="room_clean">掃除していない<br>
                 <input type = "radio" value="1" name="room_clean">チェックイン状態<br>
                 <input type = "radio" value="2" name="room_clean">掃除済み<br>
+                <input type = "hidden" value="<?php echo $room_number; ?>" name="room_number">
             </form>
             <div class="input#submit_button">
                 <div class="inline">
@@ -34,7 +35,7 @@
                         <input id="submit_button" type="submit" name="submit" value="戻る" onclick="location.href='./s_clean_management.php'"> 
                     </form>
                     <!--完了ボタン-->
-                    <input id="submit_button" type="submit" name="submit" value="完了" form = "clean_edit" value="<?php echo $room_number; ?>" name="room_number"></input>
+                    <input id="submit_button" type="submit" name="submit" value="完了" form = "clean_edit" value="<?php echo $room_number; ?>" name="room_number">
                 </div>
             </div>
         </div>
