57bd0af0cd90b5275d05e29179d1e5179968765c
sugimotowataru <sugimotowataru666@gmail.com>
2021-01-02 14:44:15 +0900

    ボタンの配置を変更

--- seisou/s_clean_edit.php
+++ seisou/s_clean_edit.php
@@ -28,17 +28,16 @@
                 <input type = "radio" value="2" name="room_clean">掃除済み<br>
                 <input type = "hidden" value="<?php echo $room_number; ?>" name="room_number">
             </form>
-            <!--戻るボタン-->
-            <div class="input#submit_button_l">    
-                <form  id = "clean_back" action = "s_clean_management.php">
-                    <input id="submit_button_l" type="submit" name="submit" value="戻る" onclick="location.href='./s_clean_management.php'"> 
-                </form>
-            </div>
-            <!--完了ボタン-->
-            <div class="input#submit_button_r">
-                <input id="submit_button_r" type="submit" name="submit" value="完了" form = "clean_edit">
-            </div>
         </div>
+        <div class="input#submit_button">
+        <!--戻るボタン-->
+            <form  id = "clean_back" action = "s_clean_management.php">
+                <input id="submit_button" type="submit" name="submit" value="戻る" onclick="location.href='./s_clean_management.php'"> 
+            </form>
+            <!--完了ボタン-->
+            <input id="submit_button" type="submit" name="submit" value="完了" form = "clean_edit">
+        </div>
+        
         <!--フッター-->
     </body>
 </html>
