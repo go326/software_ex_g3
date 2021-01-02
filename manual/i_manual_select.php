6d7e14701025bbd9a4727a55570910e78c1bca29
220299v <220299v@users.noreply.github.com>
2021-01-02 14:38:43 +0900

    1:2

--- manual/i_manual_select.php
+++ manual/i_manual_select.php
@@ -81,7 +81,7 @@
         </table>
         
         <!--戻るボタン-->
-        <form method="get" action = "i_mq_top.php">
+        <form method="get" action = "i_mq_top.html">
             <input type = "submit" value = "よくある質問、マニュアルTOP画面に戻る">
         </form>
         
