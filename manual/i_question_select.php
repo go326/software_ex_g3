a3983af8e4762eb17ae854ffac098c3ac35cbc11
220299v <220299v@users.noreply.github.com>
2021-01-02 21:46:23 +0900

    1/2

--- manual/i_question_select.php
+++ manual/i_question_select.php
@@ -34,7 +34,7 @@
                 $next_date = date("Y-m-d", strtotime("+1 day"));
                 if($user_auth == 1){
                     echo ("<ul>");
-                    echo ("<li><input type=\"button\" onclick=\"location.href=\'./i_question_insert.php?question_number=<?php echo ($NUM_OF_QUESTION + 1);?>\'\"value=\"新規入力\"></li>");
+                    echo ("<li><input type=\"button\" onclick=\"location.href='./i_question_insert.php?question_number=<?php echo ($NUM_OF_QUESTION + 1);?>'\" value=\"新規入力\"></li>");
                     echo ("</ul>");
                 }
                 ?>
