900060af751056168b6d1c2f3cd6ee1fe558940d
sugimotowataru <sugimotowataru666@gmail.com>
2021-01-02 14:59:49 +0900

    button-positionの追加

--- seisou/s_clean_edit.php
+++ seisou/s_clean_edit.php
@@ -29,15 +29,16 @@
                 <input type = "hidden" value="<?php echo $room_number; ?>" name="room_number">
             </form>
         </div>
-        <div class="input#submit_button">
-        <!--戻るボタン-->
-            <form  id = "clean_back" action = "s_clean_management.php">
-                <input id="submit_button" type="submit" name="submit" value="戻る" onclick="location.href='./s_clean_management.php'"> 
-            </form>
-            <!--完了ボタン-->
-            <input id="submit_button" type="submit" name="submit" value="完了" form = "clean_edit">
+        <div class="button-position">
+            <div class="input#submit_button">
+            <!--戻るボタン-->
+                <form  id = "clean_back" action = "s_clean_management.php">
+                    <input id="submit_button" type="submit" name="submit" value="戻る" onclick="location.href='./s_clean_management.php'"> 
+                </form>
+                <!--完了ボタン-->
+                <input id="submit_button" type="submit" name="submit" value="完了" form = "clean_edit">
+            </div>
         </div>
-        
         <!--フッター-->
     </body>
 </html>
