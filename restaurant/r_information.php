cad7f07efd44799dcc2bf4b23785957b7c55ed4d
220303m <220303m@ugs.kochi-tech.ac.jp>
2021-01-04 21:01:29 +0900

    php内部にcssを記述するように変更

--- restaurant/r_information.php
+++ restaurant/r_information.php
@@ -15,16 +15,16 @@ try {
   $stmt->execute();
   // 表の作成(基本的にhtml文と同じ)
   while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    $rinfo .= "<tr><th>";
+    $rinfo .= "<tr><td>";
     $rinfo .= "<form action='f_information_details.php' method='post'>";
     $rinfo .= "<button type='submit' name='fid' ";
     $rinfo .= "value='" . $row['reseravetion_id'] . "'>" . $row['customer_name'] . "</button>";
     $rinfo .= "</form>";
-    $rinfo .= "</th><th>";
-    $rinfo .= $row['adult'] . "</th><th>";
-    $rinfo .= $row['child'] . "</th><th>";
+    $rinfo .= "</td><td>";
+    $rinfo .= $row['adult'] . "</td><td>";
+    $rinfo .= $row['child'] . "</td><td>";
     $rinfo .= $row['dinner_menu'];
-    $rinfo .= "</th></tr>";
+    $rinfo .= "</td></tr>";
   }
 } catch (PDOException $e) {
   echo $e->getMessage();
@@ -39,13 +39,38 @@ try {
   <!--文字コードUTF-8-->
   <meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″>
   <title> </title>
-  <link rel="stylesheet" href="restaurant.css" type="text/css">
+ <!--  <link rel="stylesheet" href="restaurant.css" type="text/css"> -->
+<style type="text/css">
+header{
+    width: 80%;
+    margin: 0 auto;
+}
+
+footer{
+    width: 110%;
+    mergin: 0 auto;
+}
+table{
+    margin: 0 auto;
+    width: 70%;
+}
+
+th.name{width: 300px};
+th.adult{width: 100px};
+th.chilt{width: 100px};
+th.menu{width: 300px};
+td{
+    font-size: 1.5em
+}
+
+</style>
   <script type="text/javascript" src="restaurant.js"></script>
 </head>
 
 <body>
   <header>
     <h1>食事情報閲覧画面</h1>
+    <script type="text/javascript">date();</script>
   </header>
 
   <!--メイン-->
@@ -53,10 +78,10 @@ try {
     <!--食事情報閲覧画面-->
     <table border="1">
       <tr valign="top" ;>
-        <th>名前</th>
-        <th>大人</th>
-        <th>子供</th>
-        <th>メニュー</th>
+        <th class="name">名前</th>
+        <th class="adult">大人</th>
+        <th class="child">子供</th>
+        <th class="menu">メニュー</th>
       </tr>
 	<!-- 表の表示 -->
       <?php echo $rinfo; ?>
