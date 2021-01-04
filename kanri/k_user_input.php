02164185d97f42d021be546da1173429731c8373
CentOS6 <syuya@localhost.localdomain>
2021-01-05 03:33:36 +0900

    1/5

---
+++ kanri/k_user_input.php
@@ -0,0 +1,35 @@
+<?php require("k_user_management.php"); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+
+<body>
+  <h1>新規ユーザ情報入力</h1>
+
+  <!-- 入力欄 -->
+  <form action="" method="post">
+    <p>ユーザID<input type="text" name="kui_id"></p>
+    <p>ユーザ名<input type="text" name="kui_name"></p>
+    <p>パスワード<input type="password" name="kui_pass"></p>
+
+    <p>ユーザ権限</p>
+    <p>
+      フロント<input type="checkbox" name="kui_auth[]" value="1">
+      清掃<input type="checkbox" name="kui_auth[]" value="2">
+      レストラン<input type="checkbox" name="kui_auth[]" value="3">
+      アルバイト<input type="checkbox" name="kui_auth[]" value="4">
+      管理者<input type="checkbox" name="kui_auth[]" value="5">
+    </p>
+
+    <?php KUserInputP(); ?>
+    <input type="button" onclick="location.href='k_user_screen.php'" value="取消">
+    <input type="submit" name="ku_input" value=" 登録">
+  </form>
+
+</body>
+
+</html>
