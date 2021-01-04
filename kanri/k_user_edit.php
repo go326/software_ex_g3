22109ee14f0b70a19d379be3b97639bb41f796ca
CentOS6 <syuya@localhost.localdomain>
2021-01-05 03:38:23 +0900

    1/5

---
+++ kanri/k_user_edit.php
@@ -0,0 +1,42 @@
+<?php
+require("k_user_management.php");
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+
+<body>
+
+  <form action="" method="post">
+    <p>ユーザID<input type="text" name="kuu_id"></p>
+    <p>ユーザ名<input type="text" name="kuu_name"></p>
+    <p>パスワード<input type="password" name="kuu_pass"></p>
+
+    <p>ユーザ権限</p>
+    <p>
+      フロント<input type="checkbox" name="kuu_auth[]" value="1">
+      清掃<input type="checkbox" name="kuu_auth[]" value="2">
+      レストラン<input type="checkbox" name="kuu_auth[]" value="3">
+      アルバイト<input type="checkbox" name="kuu_auth[]" value="4">
+      管理者<input type="checkbox" name="kuu_auth[]" value="5">
+    </p>
+
+
+    <p>
+      <input type="button" onclick="location.href='k_user_screen.php'" value="取消">
+      <?php echo "<button type='submit' name='ku_edit' value='" . $_POST['kid'] . "'>登録</button>"; ?>
+      <?php echo "<button type='submit' name='ku_del' value='" . $_POST['kid'] . "'>削除</button>"; ?>
+  </form>
+
+  <?php KUserEditP(); ?>
+  <?php KUserDelP(); ?>
+
+  </p>
+
+</body>
+
+</html>
