1d1d6626c58486fcdd96d73ba009e11046d652c8
go326 <220321r@ugs.kochi-tech.ac.jp>
2021-01-07 02:58:28 +0900

    f_search.phpを編集しました。

--- front/f_search/f_search.php
+++ front/f_search/f_search.php
@@ -1,45 +1,52 @@
-
 <?php
 include("../../db_connect.php");
 
 global $pdo;
 var_dump($_POST);
 if (isset($_POST['search'])) {
-    if ($_POST['reservation']  == 'past') {
-        $sql = "SELECT * FROM past_customer where ";
-        if (!empty($_POST['tel']) && !empty($_POST['name'])) {
-            $sql .= " phone_number =" . $_POST['tel'] . "and customer_name = " . $_POST['name'];
-        } else if (!empty($_POST['name'])) {
-            $sql .= " customer_name =" . $_POST['name'];
-        } else if (!empty($_POST['tel'])) {
-            $sql .= " phone_number =" . $_POST['tel'];
-        }
-    } else if ($_POST['reservation']  == 'future') {
-        $sql = "SELECT * FROM customer where";
-        echo $sql;
-        if (!empty($_POST['tel']) && !empty($_POST['name'])) {
-            $sql .= " phone_number =" . $_POST['tel'] . "and customer_name = " . $_POST['name'];
-        } else if (!empty($_POST['name'])) {
-            $sql .= " customer_name =" . $_POST['name'];
-        } else if (!empty($_POST['tel'])) {
-            $sql .= " phone_number =" . $_POST['tel'];
-        }
-    }
+    $sql = "SELECT * FROM ? where ";
+
+    // if ($_POST['reservation']  == 'past') {
+    //     $sql = "SELECT * FROM past_customer where ";
+    //     if (!empty($_POST['tel']) && !empty($_POST['name'])) {
+    //         $sql .= " phone_number =" . $_POST['tel'] . " and customer_name like " . $_POST['name'];
+    //     } else if (!empty($_POST['name'])) {
+    //         $sql .= " customer_name like " . $_POST['name'];
+    //     } else if (!empty($_POST['tel'])) {
+    //         $sql .= " phone_number =" . $_POST['tel'];
+    //     }
+    // } else if ($_POST['reservation']  == 'future') {
+    //     $sql = "SELECT * FROM customer where";
+    //     if (!empty($_POST['tel']) && !empty($_POST['name'])) {
+    //         $sql .= " phone_number =" . $_POST['tel'] . " and customer_name like " . $_POST['name'];
+    //     } else if (!empty($_POST['name'])) {
+    //         $sql .= " customer_name like " . $_POST['name'];
+    //     } else if (!empty($_POST['tel'])) {
+    //         $sql .= " phone_number =" . $_POST['tel'];
+    //     }
+    // }
     echo $sql;
     echo "<br>";
     $smt = $pdo->query($sql);
     $data = $smt->fetchAll(PDO::FETCH_ASSOC);
     var_dump($data);
+} else {
+    header("Location:./f_search.html");
 }
 
 foreach ($data as $row) {
-    $res .= "<tr>
-                <td>" . $row['reseravetion_id'] . "</td>
-                <td>" . $row['stay_date'] . "</td>
-                <td>" . $row['reservation_date'] . "</td>
-                <td>" . $row['stay_count'] . "</td>
-                <td>" . $row['customer_name'] . "</td>
-                <td>" . $row['customer_address'] . "</td>
-                <td>" . $row['phone_number'] . "</td></th></tr>";
+?>
+    <form method="post" name="form1" action="../f_information/f_information.php">
+    <?php
+    $res .= "<tr>";
+    $res .= "<td><a href='' value=" . $row['reservation_id'] . ">" . $row['reseravetion_id'] . "</a></td>";
+    $res .= "<td>" . $row['stay_date'] . "</td>";
+    $res .= "<td>" . $row['reservation_date'] . "</td>";
+    $res .= "<td>" . $row['stay_count'] . "</td>";
+    $res .= "<td>" . $row['customer_name'] . "</td>";
+    $res .= "<td>" . $row['customer_address'] . "</td>";
+    $res .= "<td>" . $row['phone_number'] . "</td></th></tr>";
 }
 echo $res;
+    ?>
+    </form>
