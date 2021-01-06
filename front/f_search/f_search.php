842f3c5f50a34c9e539cbe0083daf210318112a1
go326 <220321r@ugs.kochi-tech.ac.jp>
2021-01-07 03:57:50 +0900

    f_search.phpを編集

--- front/f_search/f_search.php
+++ front/f_search/f_search.php
@@ -2,46 +2,54 @@
 include("../../db_connect.php");
 
 global $pdo;
+
+ini_set('display_errors', "On");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 var_dump($_POST);
-if (isset($_POST['search'])) {
-    $sql = "SELECT * FROM ? where ";
-    // if ($_POST['reservation']  == 'past') {
-    if (!empty($_POST['tel']) && !empty($_POST['name'])) {
-        $sql .= " phone_number = ? and customer_name like ?";
-        $name = "%" . $_POST['name'] . "%";
-        $smt = $pdo->prepare($sql);
-        $smt->bindValue(1, $_POST['reservation']);
-        $smt->bindValue(2, $_POST['tel'], PDO::PARAM_STR);
-        $smt->bindValue(3, $name, PDO::PARAM_STR);
-    } else if (!empty($_POST['name'])) {
-        $sql .= " customer_name like ? ";
-        $name = "%" . $_POST['name'] . "%";
-        $smt = $pdo->prepare($sql);
-        $smt->bindValue(1, $_POST['reservation']);
-        $smt->bindValue(2, $name, PDO::PARAM_STR);
-    } else if (!empty($_POST['tel'])) {
-        $sql .= " phone_number = ?";
-        $smt = $pdo->prepare($sql);
-        $smt->bindValue(1, $_POST['reservation']);
-        $smt->bindValue(2, $_POST['tel'], PDO::PARAM_STR);
+try {
+    if (isset($_POST['search'])) {
+        $sql = "SELECT * FROM ? where ";
+        // if ($_POST['reservation']  == 'past') {
+        if (!empty($_POST['tel']) && !empty($_POST['name'])) {
+            $sql .= " phone_number = ? and customer_name like ?";
+            $name = "%" . $_POST['name'] . "%";
+            $smt = $pdo->prepare($sql);
+            $smt->bindValue(1, $_POST['reservation']);
+            $smt->bindValue(2, $_POST['tel'], PDO::PARAM_STR);
+            $smt->bindValue(3, $name, PDO::PARAM_STR);
+        } else if (!empty($_POST['name'])) {
+            $sql .= " customer_name like ? ";
+            $name = "%" . $_POST['name'] . "%";
+            $smt = $pdo->prepare($sql);
+            $smt->bindValue(1, $_POST['reservation']);
+            $smt->bindValue(2, $name, PDO::PARAM_STR);
+        } else if (!empty($_POST['tel'])) {
+            $sql .= " phone_number = ?";
+            $smt = $pdo->prepare($sql);
+            $smt->bindValue(1, $_POST['reservation']);
+            $smt->bindValue(2, $_POST['tel'], PDO::PARAM_STR);
+        }
+        // } else if ($_POST['reservation']  == 'future') {
+        //     $sql = "SELECT * FROM customer where";
+        //     if (!empty($_POST['tel']) && !empty($_POST['name'])) {
+        //         $sql .= " phone_number =" . $_POST['tel'] . " and customer_name like " . $_POST['name'];
+        //     } else if (!empty($_POST['name'])) {
+        //         $sql .= " customer_name like " . $_POST['name'];
+        //     } else if (!empty($_POST['tel'])) {
+        //         $sql .= " phone_number =" . $_POST['tel'];
+        //     }
+        // }
+        echo $sql;
+        echo "<br>";
+        $smt->execute();
+        $data = $smt->fetchAll(PDO::FETCH_ASSOC);
+        var_dump($data);
+    } else {
+        header("Location:./f_search.html");
     }
-    // } else if ($_POST['reservation']  == 'future') {
-    //     $sql = "SELECT * FROM customer where";
-    //     if (!empty($_POST['tel']) && !empty($_POST['name'])) {
-    //         $sql .= " phone_number =" . $_POST['tel'] . " and customer_name like " . $_POST['name'];
-    //     } else if (!empty($_POST['name'])) {
-    //         $sql .= " customer_name like " . $_POST['name'];
-    //     } else if (!empty($_POST['tel'])) {
-    //         $sql .= " phone_number =" . $_POST['tel'];
-    //     }
-    // }
-    echo $sql;
-    echo "<br>";
-    $smt->execute();
-    $data = $smt->fetchAll(PDO::FETCH_ASSOC);
-    var_dump($data);
-} else {
-    header("Location:./f_search.html");
+} catch (PDOException $e) {
+    var_dump($e->getMessage());
 }
 
 foreach ($data as $row) {
