778d72b94ea4bdee8f120b4f183d7a57eb2ab939
go326 <220321r@ugs.kochi-tech.ac.jp>
2021-01-06 20:27:13 +0900

    room.phpを編集

--- front/room.php
+++ front/room.php
@@ -9,8 +9,159 @@ $smt = $pdo->prepare($sql);
 $data = $smt->execute();
 $data = $smt->fetchAll();
 var_dump($data);
+
+$room = array();
+foreach ($data as $value) {
+    array_push($room, $value);
+}
 ?>
-<script type="text/javascript">
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″>
+    <link rel="stylesheet" href="./clean_management.css" type="text/css">
+    <title>seisou</title>
+
+</head>
+
+<body>
+    <header>
+        <h1> 清掃情報管理画面</h1>
+
+        <!--戻るボタン-->
+        <div class="button-position-c">
+            <div class="input#submit_button">
+                <form action="../i_general_top.html">
+                    <input id="submit_button" type="submit" name="submit" value="総合TOP画面へ戻る">
+                </form>
+            </div>
+        </div>
+
+        <!--日付取得-->
+        <?php
+        $date = date("Y-m-d");
+        echo ($date . "<br>");
+        $next_date = date("Y-m-d", strtotime("+1 day"));
+        ?>
+
+        <span class="sample0"><button class="bg_color0"></button></span>掃除していない、予約なしの状態<br>
+        <span class="sample1"><button class="bg_color0"></button></span>お客様がチェックインしている状態<br>
+        <span class="sample2"><button class="bg_color0"></button></span>掃除済み状態<br>
+    </header>
+
+    <!--清掃情報確認画面の枠組みの作成-->
+    <!--formがget方式だがpostにする予定最悪このまま-->
+    <form method="P" action="s_clean_edit.php">
+        <?php
+        //３階分テーブルを作成する
+        foreach ($room as $num => $value) {
+
+            if ($value % 100 == 1) {
+                echo ("<table>");
+                //ホテルの１階分だけループする。
+                $room_count = 0; //1階の部屋数のカウント
+                echo ("<tr>");
+                //表の１行に表示する部屋数分だけループする
+            }
+            //1セルの表示開始
+            echo ("<td>");
+            //1部屋のリンク現在はボタンで作成
+            //チェックインの情報をとるかな？
+            $SCMroom_clean = SCleanManagemantP($ROOM_DATA[$table][$room_count]);
+            //bg_color0,1,2あるがこれを文字列結合で判断している。
+            echo ("<button class = room_button bg_color" . $SCMroom_clean . " type = submit value = " . $ROOM_DATA[$table][$room_count] . " name = room_number >");
+
+            //1セルの表示名
+            //1行目
+            echo ($value);
+            //改行
+            echo ("<br>");
+
+            echo cus_name($value) . "<br>";
+            //今日の宿泊者数
+            $number_people = SCleanNumberP($date, $ROOM_DATA[$table][$room_count]);
+            echo ("本日" . $number_people . "人");
+            echo ("<br>");
+            echo ("</button>");
+            echo ("</td>\n");
+            //１セル終了
+
+            if ($value[$num + 1] % 100 == 1) {
+                echo ("</tr>");
+                echo ("</table>\n");
+            }
+        }
+        ?>
+    </form>
+</body>
+
+</html>
+
+<?php
+//清掃情報確認画面の枠組みに反映
+//清掃情報確認画面の枠組みの清掃状況を取り出し
+//ここで参照する色を決めている
+function SCleanManagemantP($room_number)
+{
+    global $pdo;
+    $room_clean = 0; //清掃状況、初期値は0で宿泊予定者がいないことを示す。
+    $stmt = $pdo->query("SELECT room_clean FROM room WHERE room_number = " . $room_number);
+    //fetch
+    while ($row = $stmt->fetch()) {
+        //$room_number = $row["room_number"];
+        $room_clean = $row["room_clean"];
+        //echo($room_number.",".$room_clean."<br>");
+    }
+    return $room_clean;
+}
+
+function cus_name($room)
+{
+    global $date, $pdo;
+    $sql = "SELECT customer_name FROM customer where stay_date = ? and ( room_1 = ? or room_2 =? or room_3 = ?)";
+    $smt = $pdo->prepare($sql);
+    $smt->bindValue(1, $date, PDO::PARAM_STR);
+    $smt->bindValue(2, $room, PDO::PARAM_INT);
+    $smt->bindValue(3, $room, PDO::PARAM_INT);
+    $smt->bindValue(4, $room, PDO::PARAM_INT);
+    $data = $smt->fetch(PDO::FETCH_ASSOC);
+    return $data;
+}
+
+//宿泊人数を表示
+function SCleanNumberP($day_number, $room_number)
+{
+    global $pdo;
+    $number_people = 0; //その部屋の人数初期値は0
+    try {
+        $people_sql = "SELECT adult, child FROM customer WHERE stay_date = " . $day_number . " AND room_1 = " . $room_number;
+        //2部屋めが生まれたらこの処理が必要かも知れないそしてAND以降を（）でくくる？
+        //."OR room_2 = ".$room_number."OR room_3 = ".$room_number
+        $stmt = $pdo->query($people_sql);
+        while ($row = $stmt->fetch()) {
+            $adult = $row["adult"];
+            $child = $row["child"];
+        }
+        if (isset($adult)) {
+            if (isset($child)) {
+                //大人も子供もいる状態
+                $number_people = $adult + $child;
+            } else {
+                //大人だけいる状態
+                $number_people = $adult;
+            }
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit;
+    }
+    return $number_people;
+}
+?>
+<!-- <script type="text/javascript">
     //import Rein from "./maketable.js";
     let room = <?php echo $data; ?>;
 </script>
@@ -98,4 +249,4 @@ var_dump($data);
             }
         }
     }
-</Script>
+</Script> -->
