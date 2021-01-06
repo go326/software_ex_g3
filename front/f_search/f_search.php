006e27ac97f27774f7f515d5a87e32cff656b25b
go326 <220321r@ugs.kochi-tech.ac.jp>
2021-01-07 05:24:28 +0900

    バグ修正

--- front/f_search/f_search.php
+++ front/f_search/f_search.php
@@ -27,16 +27,6 @@ try {
             $smt = $pdo->prepare($sql);
             $smt->bindValue(':phone', $_POST['tel'], PDO::PARAM_STR);
         }
-        // } else if ($_POST['reservation']  == 'future') {
-        //     $sql = "SELECT * FROM customer where";
-        //     if (!empty($_POST['tel']) && !empty($_POST['name'])) {
-        //         $sql .= " phone_number =" . $_POST['tel'] . " and customer_name like " . $_POST['name'];
-        //     } else if (!empty($_POST['name'])) {
-        //         $sql .= " customer_name like " . $_POST['name'];
-        //     } else if (!empty($_POST['tel'])) {
-        //         $sql .= " phone_number =" . $_POST['tel'];
-        //     }
-        // }
         echo $sql;
         echo "<br>";
         $smt->execute();
@@ -54,8 +44,8 @@ foreach ($data as $row) {
 ?>
     <form method="post" name="form1" action="../f_information/f_information.php">
     <?php
-    $res .= "<tr>";
-    $res .= "<td><a href='' value=" . $row['reservation_id'] . ">" . $row['reseravetion_id'] . "</a></td>";
+    $res = "<tr>";
+    $res .= "<td><a href='' value=" . $row['reseravetion_id'] . ">" . $row['reseravetion_id'] . "</a></td>";
     $res .= "<td>" . $row['stay_date'] . "</td>";
     $res .= "<td>" . $row['reservation_date'] . "</td>";
     $res .= "<td>" . $row['stay_count'] . "</td>";
