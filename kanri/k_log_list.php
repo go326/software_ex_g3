f238c168f16dc05ee961f36c7c9c2150559fb294
CentOS6 <syuya@localhost.localdomain>
2021-01-07 08:28:15 +0900

    log list

---
+++ kanri/k_log_list.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+# var
+$dsn = 'mysql:dbname=admin;host=localhost;charset=utf8';
+$user = 'admin';
+$password = 'software_ex_g3';
+
+$sql = "";
+$res="";
+
+
+try {
+  $pdo = new PDO($dsn, $user, $password);
+  // SELECT
+  function KLogListP()
+  {
+    global $pdo, $sql, $res;
+    $sql = "SELECT * FROM log";
+    $stmt = $pdo->query($sql);
+    $stmt->execute();
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+      $res .= "<tr><td>";
+      $res .= substr($row['log_date'], 0, 10);
+      $res .= "</td><td>";
+      $res .= $row['log_name'];
+      $res .= "</td><td>";
+      $res .= $row['log_work'];
+      $res .= "</td><td>";
+      $res .= $row['log_table'];
+      $res .= "</td><td>";
+      $res .= $row['log_line'];
+      $res .= "</td><td>";
+      $res .= $row['log_attribute'];
+      $res .= "</td><td>";
+      $res .= $row['log_befor'];
+      $res .= "</td><td>";
+      $res .= $row['log_after'];
+      $res .= "</td></tr>";
+    }
+  }
+
+} catch (PDOException $e) {
+  echo $e->getMessage();
+  exit;
+  }
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>log</title>
+</head>
+
+<body>
+
+  <!-- Management -->
+  <table>
+    <?php KLogListP(); echo $res; ?>
+  </table>
+
+</body>
+
+</html>
