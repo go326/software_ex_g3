de954cc4f33132005c9219c7a1d9f17361f0b54a
220301j <220301j@ugs.kochi-tech.ac.jp>
2021-01-09 00:15:41 +0900

    2021/01/09:headにjsを追加

--- front/room.php
+++ front/room.php
@@ -22,6 +22,7 @@ foreach ($data as $value) {
     <meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″>
     <link rel="stylesheet" href="../seisou/clean_management.css" type="text/css">
     <link rel="stylesheet" href="f_top.css" type="text/css">
+    <script src="f_top.js"></script><!--今日の日付を取得-->
     <title>フロントTOP画面</title>
 
 </head>
@@ -46,9 +47,7 @@ foreach ($data as $value) {
             <li id="view_date"></li>
         </ul>
         <!--今日の日付の表示-->
-        <script type="text/javascript">
-            date();
-        </script>
+        <script type="text/javascript">date();</script>
 
 
         <!--日付取得-->
