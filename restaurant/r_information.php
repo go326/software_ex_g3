a998501578d0797e765fe4612de82d5e6f244c3e
CentOS6 <syuya@localhost.localdomain>
2021-01-11 15:26:55 +0900

    1/11

--- restaurant/r_information.php
+++ restaurant/r_information.php
@@ -12,7 +12,7 @@ try {
   $pdo = new PDO($dsn, $user, $password);
   // DBの呼び出し
   $sql = "SELECT reseravetion_id,customer_name,adult,child,dinner_menu FROM customer WHERE is_dinner = 0 AND stay_date = '$date' ";
-  echo $sql;
+
   $stmt = $pdo->query($sql);
   $stmt->execute();
   // 表の作成(基本的にhtml文と同じ)
