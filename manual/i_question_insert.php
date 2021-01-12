1bc1c2fbf85abb723bcf76139ad471985fdcc4da
220299v <220299v@users.noreply.github.com>
2021-01-13 00:53:18 +0900

    1/13

--- manual/i_question_insert.php
+++ manual/i_question_insert.php
@@ -19,7 +19,7 @@
                         alert_text = alert_text + question_result;
                     }
                     //条件に一致する場合(メールアドレスが空の場合)
-                    alert(question_name);    //エラーメッセージを出力
+                    alert(alert_text);    //エラーメッセージを出力
                     return false;    //送信ボタン本来の動作をキャンセルします
                 }else{
                     //条件に一致しない場合(メールアドレスが入力されている場合)
