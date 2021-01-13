fe8c2a0d12f8086510fbe4f050747d08345dc51f
EC2 Default User <ec2-user@ip-172-31-80-68.ec2.internal>
2021-01-13 09:22:23 +0000

    1/13

--- kanri/k_user_edit.php
+++ kanri/k_user_edit.php
@@ -16,7 +16,7 @@
     <header>
     <div id="main">
         <form action="" method="post">
-        <p>ユーザID  :<input type="text" name="kuu_id" minlength="1" maxlength="12" required></p>
+        <p>ユーザID  :<input type="number" name="kuu_id" minlength="1" maxlength="12" required></p>
         <p>ユーザ名  :<input type="text" name="kuu_name" minlength="1" maxlength="16" required></p>
         <p>パスワード:<input type="password" name="kuu_pass" minlength="1" required></p>
 
