617f09ec8040f4f3b0133ed74d2fb420a0476709
EC2 Default User <ec2-user@ip-172-31-80-68.ec2.internal>
2021-01-13 07:50:58 +0000

    1/13

--- kanri/k_log_record.php
+++ kanri/k_log_record.php
@@ -1,34 +1,34 @@
-<?php
-session_start();
-# var
-$dsn = 'mysql:dbname=admin;host=localhost;charset=utf8';
-$user = 'admin';
-$password = 'software_ex_g3';
+    <?php
+    session_start();
+    # var
+    $dsn = 'mysql:dbname=admin;host=localhost;charset=utf8';
+    $user = 'admin';
+    $password = 'software_ex_g3';
 
-$sql = "";
-$date = "";
-$date = date('Y-m-d H:i:s');
-$name="";
-//$name = $_SESSION['user'];
-$name = "admin";
+    $sql = "";
+    $date = "";
+    $date = date('Y-m-d H:i:s');
+    $name="";
+    //$name = $_SESSION['user'];
+    $name = "admin";
 
-try {
-  $pdo = new PDO($dsn, $user, $password);
+    try {
+        $pdo = new PDO($dsn, $user, $password);
 
-  // INSERT
-  function KLogRecodeP($work,$table,$line,$attribute,$befor,$after)
-  {
-    global $pdo, $sql,$date,$name;
-    if($name != "" and $work != "" and $table != "" and $line != "" and $attribute != "" and $befor != "" and $after != ""){
-      $sql = "INSERT INTO log VALUES('$date','$name','$work','$table','$line','$attribute','$befor','$after')";
-      //  echo $sql;
-      $stmt = $pdo->prepare($sql);
-      $stmt->execute();
-    }
-  }
+        // INSERT
+        function KLogRecodeP($work,$table,$line,$attribute,$befor,$after)
+        {
+        global $pdo, $sql,$date,$name;
+        if($name != "" and $work != "" and $table != "" and $line != "" and $attribute != "" and $befor != "" and $after != ""){
+            $sql = "INSERT INTO log VALUES('$date','$name','$work','$table','$line','$attribute','$befor','$after')";
+            //  echo $sql;
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute();
+        }
+        }
 
-} catch (PDOException $e) {
-  echo $e->getMessage();
-  exit;
-  }
-?>
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit;
+        }
+    ?>
