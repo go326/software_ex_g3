c6fbd0e612400a4c5c5cde2bddb0237712875d13
EC2 Default User <ec2-user@ip-172-31-80-68.ec2.internal>
2021-01-13 07:50:16 +0000

    1/13

--- kanri/k_user_screen.php
+++ kanri/k_user_screen.php
@@ -1,29 +1,29 @@
-<?php require("k_user_management.php"); ?>
-<!DOCTYPE html>
-<html>
+    <?php require("k_user_management.php"); ?>
+    <!DOCTYPE html>
+    <html>
 
-<head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="./k_user.css" type="text/css">
-  <title>screen</title>
-  
-</head>
+    <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./k_user.css" type="text/css">
+    <title>screen</title>
+    
+    </head>
 
-<body>
-<header>
-  <p><input type="button" onclick="location.href='k_top.html'" value="TOPへ戻る"></p>
-  <h1>ユーザ情報管理</h1> 
-  <p><input type="button" onclick="location.href='k_user_input.php'" value="新規入力"></p>
-</header> 
-  <!-- 一覧 -->
-  <table border="1">
-    <tr>
-      <th> ユーザID </th>
-      <th> ユーザ名 </th>
-    </tr>
-    <?php KUserManagementP();
-    echo $k_res; ?>
-  </table>
-</body>
+    <body>
+    <header>
+    <p><input type="button" onclick="location.href='k_top.html'" value="TOPへ戻る"></p>
+    <h1>ユーザ情報管理</h1> 
+    <p><input type="button" onclick="location.href='k_user_input.php'" value="新規入力"></p>
+    </header> 
+    <!-- 一覧 -->
+    <table border="1">
+        <tr>
+        <th> ユーザID </th>
+        <th> ユーザ名 </th>
+        </tr>
+        <?php KUserManagementP();
+        echo $k_res; ?>
+    </table>
+    </body>
 
-</html>
+    </html>
