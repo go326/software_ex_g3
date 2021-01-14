ac01b3a5abb3daea16502f9214dae5bf215e33dd
EC2 Default User <ec2-user@ip-172-31-80-68.ec2.internal>
2021-01-14 08:51:46 +0000

    1/14

--- kanri/k_user_edit.php
+++ kanri/k_user_edit.php
@@ -3,47 +3,46 @@
     ?>
     <!DOCTYPE html>
     <html>
-        
+
     <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="./k_user.css" type="text/css">
-    <title></title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="./k_user.css" type="text/css">
+        <title></title>
     </head>
 
     <body>
-    <header>
-        <h1>ユーザ情報変更</h1>
-    <header>
-    <div id="main">
-        <form action="" method="post">
-        <p>ユーザID  :<input type="number" name="kuu_id" minlength="1" maxlength="12" required></p>
-        <p>ユーザ名  :<input type="text" name="kuu_name" minlength="1" maxlength="16" required></p>
-        <p>パスワード:<input type="password" name="kuu_pass" minlength="1" maxlength="16" required></p>
+        <header>
+            <h1>ユーザ情報変更</h1>
+            <header>
+                <div id="main">
+                    <form action="" method="post">
+                        <p>ユーザID :<input type="number" name="id" minlength="1" maxlength="12" required></p>
+                        <p>ユーザ名 :<input type="text" name="name" minlength="1" maxlength="16" required></p>
+                        <p>パスワード:<input type="password" name="pass" minlength="1" maxlength="16" required></p>
 
-        <p>ユーザ権限</p>
-        <p>
-            フロント<input type="checkbox" name="kuu_auth[]" value="1">
-            清掃<input type="checkbox" name="kuu_auth[]" value="2">
-            レストラン<input type="checkbox" name="kuu_auth[]" value="3">
-            アルバイト<input type="checkbox" name="kuu_auth[]" value="4">
-            管理者<input type="checkbox" name="kuu_auth[]" value="5">
-        </p>
+                        <p>ユーザ権限</p>
+                        <p>
+                            フロント<input type="checkbox" name="auth[]" value="1">
+                            清掃<input type="checkbox" name="auth[]" value="2">
+                            レストラン<input type="checkbox" name="auth[]" value="3">
+                            アルバイト<input type="checkbox" name="auth[]" value="4">
+                            管理者<input type="checkbox" name="auth[]" value="5">
+                        </p>
 
+                        <p>
+                            <input type="button" onclick="location.href='k_user_screen.php'" value="取消">
+                            <input type="submit" name="edit" value="登録">
+                        <p>
+                    </form>
+                    <form action="" method="post">
+                        <input type="submit" name="del" value="削除">
+                    </form>
 
-        <p>
-        <input type="button" onclick="location.href='k_user_screen.php'" value="取消">
-        <input type="submit" name="ku_edit" value="登録">
-        <p>
-        </form>
-        <form action="" method="post">
-        <input type="submit" name="ku_del" value="削除">
-        </form>
+                    <?php KUserEditP(); ?>
+                    <?php KUserDelP(); ?>
 
-        <?php KUserEditP(); ?>
-        <?php KUserDelP(); ?>
-
-        </p>
-    </div>
+                    </p>
+                </div>
     </body>
 
     </html>
