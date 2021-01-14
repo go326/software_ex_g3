72dc4a07145b6d35af91bdbd1381d7d6b288a639
EC2 Default User <ec2-user@ip-172-31-80-68.ec2.internal>
2021-01-14 09:17:39 +0000

    1/14

--- kanri/k_user_edit.php
+++ kanri/k_user_edit.php
@@ -16,7 +16,7 @@
             <header>
                 <div id="main">
                     <form action="" method="post">
-                        <p>ユーザID :<input type="number" name="id" value="<?php echo $eid ?>" minlength="1" maxlength="12" required></p>
+                        <p>ユーザID :<input type="number" name="id" value="<?php echo $_SESSION['eid'] ?>" minlength="1" maxlength="12" required></p>
                         <p>ユーザ名 :<input type="text" name="name" minlength="1" maxlength="16" required></p>
                         <p>パスワード:<input type="password" name="pass" minlength="1" maxlength="16" required></p>
 
