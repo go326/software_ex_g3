7a8061b3a282eec665a07fea0e9111aef90346a0
EC2 Default User <ec2-user@ip-172-31-80-68.ec2.internal>
2021-01-14 07:41:59 +0000

    1/14

--- restaurant/r_information.php
+++ restaurant/r_information.php
@@ -11,37 +11,31 @@ $date = $dt->format('Y-m-d');
 $room = "";
 
 try {
-  $pdo = new PDO($dsn, $user, $password);
+    $pdo = new PDO($dsn, $user, $password);
 
-  $sql = "SELECT room_1 FROM customer";
-  $stmt = $pdo->query($sql);
-  $stmt->execute();
-  $row = $stmt->fetch();
-  $room = $row['room_1'];
+    // SELECT
+    $sql = "SELECT * FROM customer WHERE is_dinner = 1";
+    $stmt = $pdo->query($sql);
+    $stmt->execute();
 
-  bool_stay($date,$room);
-
-  // SELECT
-  $sql = "SELECT reseravetion_id,customer_name,adult,child,dinner_menu FROM customer WHERE is_dinner = 0 AND stay_date = '$date' ";
-  $stmt = $pdo->query($sql);
-  $stmt->execute();
-  
-  // 表の表示
-  while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    $rinfo .= "<tr><td>";
-    $rinfo .= "<form action='f_information_details.php' method='post'>";
-    $rinfo .= "<button type='submit' name='fid' ";
-    $rinfo .= "value='{$row['reseravetion_id']}'> {$row['customer_name']} </button>";
-    $rinfo .= "</form>";
-    $rinfo .= "</td><td>";
-    $rinfo .= "{$row['adult']}</td><td>";
-    $rinfo .= "{$row['child']}</td><td>";
-    $rinfo .= "{$row['dinner_menu']}";
-    $rinfo .= "</td></tr>";
-  }
+    // 表の表示
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $room = $row['room_1'];
+        bool_stay($date, $room);
+        $rinfo .= "<tr><td>";
+        $rinfo .= "<form action='f_information_details.php' method='post'>";
+        $rinfo .= "<button type='submit' name='fid' ";
+        $rinfo .= "value='{$row['reseravetion_id']}'> {$row['customer_name']} </button>";
+        $rinfo .= "</form>";
+        $rinfo .= "</td><td>";
+        $rinfo .= "{$row['adult']}</td><td>";
+        $rinfo .= "{$row['child']}</td><td>";
+        $rinfo .= "{$row['dinner_menu']}";
+        $rinfo .= "</td></tr>";
+    }
 } catch (PDOException $e) {
-  echo $e->getMessage();
-  exit;
+    echo $e->getMessage();
+    exit;
 }
 ?>
 
@@ -49,68 +43,88 @@ try {
 <html>
 
 <head>
-  <!--文字コードUTF-8-->
-  <meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″>
-  <title>食事情報閲覧画面</title>
- <!--  <link rel="stylesheet" href="restaurant.css" type="text/css"> -->
-<style type="text/css">
-header{
-    width: 80%;
-    margin: 0 auto;
-}
+    <!--文字コードUTF-8-->
+    <meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″>
+    <title>食事情報閲覧画面</title>
+    <!--  <link rel="stylesheet" href="restaurant.css" type="text/css"> -->
+    <style type="text/css">
+        header {
+            width: 80%;
+            margin: 0 auto;
+        }
 
-footer{
-    width: 110%;
-    margin: 0 auto;
-}
-table{
-    margin: 0 auto;
-    width: 60%;
-}
+        footer {
+            width: 110%;
+            margin: 0 auto;
+        }
 
-table th.name{width: 30%};
-table th.adult{width: 20%};
-table th.chilt{width: 20%};
-table th.menu{width: 30%};
-table tr td{
-    font-size: 1.5em;
-}
+        table {
+            margin: 0 auto;
+            width: 60%;
+        }
 
-input{
-    font-size: 1.3em;
-}
+        table th.name {
+            width: 30%
+        }
 
-</style>
-<script type="text/javascript"></script>
+        ;
+
+        table th.adult {
+            width: 20%
+        }
+
+        ;
+
+        table th.chilt {
+            width: 20%
+        }
+
+        ;
+
+        table th.menu {
+            width: 30%
+        }
+
+        ;
+
+        table tr td {
+            font-size: 1.5em;
+        }
+
+        input {
+            font-size: 1.3em;
+        }
+    </style>
+    <script type="text/javascript"></script>
 
 </head>
 
 <body>
-  <header>
-    <h1>食事情報閲覧画面</h1>
-  </header>
+    <header>
+        <h1>食事情報閲覧画面</h1>
+    </header>
 
-  <!--メイン-->
-  <div id="main">
-    <!--食事情報閲覧画面-->
-    <table border="1">
-      <tr valign="top">
-        <th class="name">名前</th>
-        <th class="adult">大人</th>
-        <th class="child">子供</th>
-        <th class="menu">メニュー</th>
-      </tr>
-	<!-- 表の表示 -->
-      <?php echo $rinfo; ?>
-    </table>
-  </div>
-  <!--フッター-->
-  <footer>
-    <br><br>
-    <center>
-      <input type="button" onclick="location.href='../i_general_top.html'" value="戻る">
-    </center>
-  </footer>
+    <!--メイン-->
+    <div id="main">
+        <!--食事情報閲覧画面-->
+        <table border="1">
+            <tr valign="top">
+                <th class="name">名前</th>
+                <th class="adult">大人</th>
+                <th class="child">子供</th>
+                <th class="menu">メニュー</th>
+            </tr>
+            <!-- 表の表示 -->
+            <?php echo $rinfo; ?>
+        </table>
+    </div>
+    <!--フッター-->
+    <footer>
+        <br><br>
+        <center>
+            <input type="button" onclick="location.href='../i_general_top.html'" value="戻る">
+        </center>
+    </footer>
 </body>
 
 </html>
