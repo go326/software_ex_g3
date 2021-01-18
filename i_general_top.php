2fed4489a8e81cb7bb4e8352843b876c27dfcdbb
220299v <220299v@users.noreply.github.com>
2021-01-18 20:40:16 +0900

    1/18

--- i_general_top.php
+++ i_general_top.php
@@ -1,5 +1,13 @@
 <?php
 $auth = $_POST['auth'];
-
 echo ($auth);
+
+function IAuthCheckP($auth,$auth_check){
+
+    if(strpos($manual_file_name, $auth_check) !== false){    
+        return true;
+    }else{
+        return false;
+    }
+}
 ?>
