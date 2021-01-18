7666aa567515f11c212f06465675180b9d8cd57f
EC2 Default User <ec2-user@ip-172-31-80-68.ec2.internal>
2021-01-18 07:27:39 +0000

    1/18

--- front/f_addfee/f_addfee_edit.php
+++ front/f_addfee/f_addfee_edit.php
@@ -31,37 +31,37 @@
     $date = date('Y-m-d H:i:s');
 
     // 予約IDの保存
-    $_SESSION['fee_id'] = "2"; //（仮）予約ID
+    $_SESSION['add_fee'] = "2"; //（仮）予約ID
     //$_SESSION['fee_id'] = $_POST['fae'];
-    $id = $_SESSION['fee_id'];
+    $id = $_SESSION['add_fee'];
 
 
     try {
-    $pdo = new PDO($dsn, $user, $password);
+        $pdo = new PDO($dsn, $user, $password);
 
-    function FeeSelectP()
-    {
-        global $pdo, $rf_sql, $res_room, $res_name, $res_date, $id, $date;
+        function FeeSelectP()
+        {
+            global $pdo, $rf_sql, $res_room, $res_name, $res_date, $id, $date;
 
-        // データの取得
-        $rf_sql = "SELECT * FROM customer WHERE customer.reseravetion_id = '$id'";
-        $stmt = $pdo->query($rf_sql);
-        $stmt->execute();
-        $row = $stmt->fetch();
+            // データの取得
+            $rf_sql = "SELECT * FROM customer WHERE customer.reseravetion_id = '$id'";
+            $stmt = $pdo->query($rf_sql);
+            $stmt->execute();
+            $row = $stmt->fetch();
 
-        // 表示データの格納
-        $res_room = $row['room_1'];
-        $res_name = $row['customer_name'];
-        $res_date = substr($date, 0, 10);
+            // 表示データの格納
+            $res_room = $row['room_1'];
+            $res_name = $row['customer_name'];
+            $res_date = substr($date, 0, 10);
 
-        // セッションに保存
-        $_SESSION['fee_room'] = $row['room_1'];
-        $_SESSION['fee_name'] = $row['customer_name'];
-        $_SESSION['fee_date'] = $date;
-    }
+            // セッションに保存
+            $_SESSION['fee_room'] = $row['room_1'];
+            $_SESSION['fee_name'] = $row['customer_name'];
+            $_SESSION['fee_date'] = $date;
+        }
     } catch (PDOException $e) {
-    echo $e->getMessage();
-    exit;
+        echo $e->getMessage();
+        exit;
     }
     ?>
 
@@ -70,48 +70,48 @@
     <html>
 
     <head>
-    <!--文字コードUTF-8-->
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" href="../f_reservation/f_input.css" type="text/css">
-    <script src="f_addfee.js"></script>
+        <!--文字コードUTF-8-->
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="../f_reservation/f_input.css" type="text/css">
+        <script src="f_addfee.js"></script>
     </head>
 
     <body>
-    <!--ヘッダー-->
-    <header>
-        <h1>追加料金編集画面</h1>
-    </header>
+        <!--ヘッダー-->
+        <header>
+            <h1>追加料金編集画面</h1>
+        </header>
 
 
-    <!-- DBからの取得 -->
-    <?php FeeSelectP(); ?>
+        <!-- DBからの取得 -->
+        <?php FeeSelectP(); ?>
 
-    <!--メイン-->
-    <div id="main">
-        <form method="POST" action="f_addfee_confimation.php">
-        <dl>
+        <!--メイン-->
+        <div id="main">
+            <form method="POST" action="f_addfee_confimation.php">
+                <dl>
 
-            <dt>部屋番号</dt>
-            <dd><?php echo $res_room; ?></dd>
-            <dt>氏名</dt>
-            <dd><?php echo $res_name; ?></dd>
-            <dt>日付</dt>
-            <dd><?php echo $res_date; ?></dd>
-            <dt>場所</dt>
-            <dd><input type="text" id="place" name="place"></dd>
-            <dt>追加料金(円)</dt>
-            <dd><input type="text" id="fee" name="fee"></dd>
-            <dt>内容</dt>
-            <dd><input type="text" id="content" name="content"></dd>
-            <dt>備考</dt>
-            <dd><input type="text" name="remark"></dd>
-        </dl>
-        <ul>
-            <li><input type="button" onclick="location.href='../f_information/f_information_details.html'" value="戻る"></li>
-            <li><input type="submit" name="edit" value="確認" onclick="return check()"></li>
-        </ul>
-        </form>
-    </div>
+                    <dt>部屋番号</dt>
+                    <dd><?php echo $res_room; ?></dd>
+                    <dt>氏名</dt>
+                    <dd><?php echo $res_name; ?></dd>
+                    <dt>日付</dt>
+                    <dd><?php echo $res_date; ?></dd>
+                    <dt>場所</dt>
+                    <dd><input type="text" id="place" name="place"></dd>
+                    <dt>追加料金(円)</dt>
+                    <dd><input type="text" id="fee" name="fee"></dd>
+                    <dt>内容</dt>
+                    <dd><input type="text" id="content" name="content"></dd>
+                    <dt>備考</dt>
+                    <dd><input type="text" name="remark"></dd>
+                </dl>
+                <ul>
+                    <li><input type="button" onclick="location.href='../f_information/f_information_details.html'" value="戻る"></li>
+                    <li><input type="submit" name="edit" value="確認" onclick="return check()"></li>
+                </ul>
+            </form>
+        </div>
     </body>
 
     </html>
