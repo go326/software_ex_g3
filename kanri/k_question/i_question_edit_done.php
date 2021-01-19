756b37a859193d5a9c74cde49fb28157a06bae62
220299v <220299v@users.noreply.github.com>
2021-01-19 22:00:40 +0900

    1/19

--- kanri/k_question/i_question_edit_done.php
+++ kanri/k_question/i_question_edit_done.php
@@ -9,7 +9,6 @@ function IQuestionEditP($question_number,$question_name,$question_result){
         $iqe_sql = "UPDATE question SET question_name = \"".$question_name."\" ,question_result = \"".$question_result."\" WHERE question_number = ".$question_number;
         $stmt = $pdo -> prepare($iqe_sql);
         $stmt -> execute();
-        echo ($iqe_sql);
         echo("<div class=\"button-area\">");    //css始まり
         echo ("実行に成功しました。<br>");
         echo ("質問No.".$question_number."を<br>");
