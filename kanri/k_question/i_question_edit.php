dd1c46474e5770adfce98707012d4157eab3cbbe
sugimotowataru <sugimotowataru666@gmail.com>
2021-01-21 18:38:22 +0900

    hederの範囲を下まで広げた

--- kanri/k_question/i_question_edit.php
+++ kanri/k_question/i_question_edit.php
@@ -45,73 +45,74 @@
         <!--ヘッダー-->
         <header>
             <h1>よくある質問編集画面</h1>
+        
+            <!--確認用の出力文＿-->
+            <div class="button-area">
+                <?php
+                    echo ("質問No.".$question_number."を変更します。");
+                ?>
+                <!--メイン-->
+                <br>
+                <!--i行目-->
+                <form id = "question_edit" method = “get” action = "i_question_edit_done.php">
+
+                    <table>
+                        <tr>
+                        <th>質問No.</th>
+                        <th>よくある質問</th>
+                        <th>解答例</th>
+                        </tr>
+
+                        <tr>    
+                        <td>
+                        <?php
+                            //0セル
+                            echo ($question_number);
+
+                        ?>
+                        <input type = "hidden" name = "question_number" value = "<?php echo ($question_number);?>">
+                        </td>
+
+                        <td>
+                            <?php
+                                //1セル
+                                $question_name = ("question_name");
+                                $question_text = IQuestionManagemantP($question_name, $question_number);
+                                echo ("<input type = \"text\" name = ".$question_name." value = ".$question_text.">");
+                            ?>
+                        </td>
+
+                        <td>
+                            <?php
+                                //2セル
+                                $question_result = ("question_result");
+                                $question_text = IQuestionManagemantP($question_result, $question_number);
+                                echo ("<input type = \"text\" name = ".$question_result." value = ".$question_text.">");
+                            ?>
+                        </td>
+
+                        </tr>
+                    </table>
+
+                </form>
+                <p>
+                <div class="button-position-l">
+                    <div class="input#submit_button">
+                        <!--戻るボタン-->
+                        <form  id = "clean_back" action = "i_question_select.php">
+                            <input id="submit_button" type="submit" name="submit" value="戻る" > 
+                        </form>
+                    </div>
+                </div>
+                <div class="button-position-r">
+                    <div class="input#submit_button">
+                        <!--完了ボタン-->
+                        <input id="submit_button" type="submit" name="submit" value="完了" form = "question_edit" onclick = "return check()">
+                    </div>
+                </div>
+                </p>
+            </div>
         </header>
-        <!--確認用の出力文＿-->
-        <div class="button-area">
-            <?php
-                echo ("質問No.".$question_number."を変更します。");
-            ?>
-            <!--メイン-->
-            <br>
-            <!--i行目-->
-            <form id = "question_edit" method = “get” action = "i_question_edit_done.php">
-
-                <table>
-                    <tr>
-                    <th>質問No.</th>
-                    <th>よくある質問</th>
-                    <th>解答例</th>
-                    </tr>
-
-                    <tr>    
-                    <td>
-                    <?php
-                        //0セル
-                        echo ($question_number);
-
-                    ?>
-                    <input type = "hidden" name = "question_number" value = "<?php echo ($question_number);?>">
-                    </td>
-
-                    <td>
-                        <?php
-                            //1セル
-                            $question_name = ("question_name");
-                            $question_text = IQuestionManagemantP($question_name, $question_number);
-                            echo ("<input type = \"text\" name = ".$question_name." value = ".$question_text.">");
-                        ?>
-                    </td>
-
-                    <td>
-                        <?php
-                            //2セル
-                            $question_result = ("question_result");
-                            $question_text = IQuestionManagemantP($question_result, $question_number);
-                            echo ("<input type = \"text\" name = ".$question_result." value = ".$question_text.">");
-                        ?>
-                    </td>
-
-                    </tr>
-                </table>
-
-            </form>
-            <p>
-            <div class="button-position-l">
-                <div class="input#submit_button">
-                    <!--戻るボタン-->
-                    <form  id = "clean_back" action = "i_question_select.php">
-                        <input id="submit_button" type="submit" name="submit" value="戻る" > 
-                    </form>
-                </div>
-            </div>
-            <div class="button-position-r">
-                <div class="input#submit_button">
-                    <!--完了ボタン-->
-                    <input id="submit_button" type="submit" name="submit" value="完了" form = "question_edit" onclick = "return check()">
-                </div>
-            </div>
-            </p>
-        </div>
         <!--フッター-->
     </body>
 </html>
