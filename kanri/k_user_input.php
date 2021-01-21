69f02d6e930207642c97f65501c16c3418ae2123
EC2 Default User <ec2-user@ip-172-31-80-68.ec2.internal>
2021-01-21 14:45:27 +0000

    1/21

--- kanri/k_user_input.php
+++ kanri/k_user_input.php
@@ -15,10 +15,15 @@
         <div id="main">
             <!-- 入力欄 -->
             <form action="" method="post">
-                <p>ユーザID　　:<input type="number" name="id" minlength="1" maxlength="12" required></p>
-                <p>ユーザ名　　:<input type="text" name="name" minlength="1" maxlength="16" required></p>
-                <p>パスワード　:<input type="password" name="pass" minlength="1" maxlength="16" required></p>
-
+                <p>ユーザID　　:<input oninput="maxLengthCheck(this)" type="number" name="id" min="0" maxlength="10" required></p>
+                <p>ユーザ名　　:<input oninput="maxLengthCheck(this)" type="text" name="name" minlength="1" maxlength="16" required></p>
+                <p>パスワード　:<input oninput="maxLengthCheck(this)" type="password" name="pass" minlength="1" maxlength="16" required></p>
+                <script>
+                    function maxLengthCheck(object) {
+                        if (object.value.length > object.maxLength)
+                            object.value = object.value.slice(0, object.maxLength)
+                    }
+                </script>
                 <p>ユーザ権限</p>
                 <p>
                     フロント<input type="checkbox" name="auth[]" value="1">
