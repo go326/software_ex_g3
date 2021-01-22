6b9a49f298f811c9021ba0da6368a7bbd157068c
EC2 Default User <ec2-user@ip-172-31-80-68.ec2.internal>
2021-01-22 03:20:50 +0000

    retirect

--- i_login.php
+++ i_login.php
@@ -24,38 +24,68 @@ if (isset($_POST['login']) && !empty($_POST['UserID']) && !empty($_POST['Passwor
         // パスワード比較
         if (isset($name)) {
             if (password_verify($_POST['Password'], $pass_db)) {
-                $text = "ログイン認証に成功";
                 $_SESSION['user'] = $_POST['UserID'];
-
-                echo ("<form method = \"post\" action = \"./i_general_top.php\">");
-                echo ("<input type = \"hidden\" name = \"auth\" value = \"" . $auth . "\">");
-                echo ("<button type=\"submit\">");
-                echo ("権限確認を表示");
-                echo ("</button>");
-                echo ("</form>");
-
-                //戻るボタン
-                $back_URL = "i_login.html";
-                echo ("<form action = " . $back_URL . ">");
-                echo ("<input id=\"submit_button\" type=\"submit\" name=\"submit\" value=\"戻る\">");
-                echo ("</form>");
-                // header("Location:./i_general_top.html");
+                $_SESSION['auth'] = $row['authority'];
+                // echo ("<form method = \"post\" action = \"./i_general_top.php\">");
+                // echo ("<input type = \"hidden\" name = \"auth\" value = \"" . $auth . "\">");
+                // echo ("<button type=\"submit\">");
+                // echo ("権限確認を表示");
+                // echo ("</button>");
+                // echo ("</form>");
+                header("Location:./i_general_top.html");
+                exit;
             } else {
-                "<script type='text/javascript'> window.onload = error_id() </script>";
-
-                header("Location:./i_login.html");
+                $alert = "<script type='text/javascript'>alert('ログイン認証に失敗しました'); </script>";
             }
         } else {
-            header("Location:./i_login.html");
+            $alert = "<script type='text/javascript'>alert('ユーザ認証に失敗しました'); </script>";
         }
     } catch (PDOException $e) {
         var_dump('Error:' . $e->getMessage());
         die();
     }
-    // ログアウト処理
-} else if (isset(($_POST['logout']))) {
-    session_unset();
-} else {
-    // "<script type='text/javascript'>window.sessionStorage.setItem(['login_error'],['入力してください']);</script>";
 }
-// header("Location:./i_login.html");
+// ログアウト処理
+if (isset(($_POST['logout']))) {
+    session_unset();
+}
+//  else {
+//     $alert = "<script type='text/javascript'>alert('入力してください'); </script>";
+// }
+echo $alert;
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <!--文字コードUTF-8-->
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="i_general_top.css">
+    <script src="i_login.js"></script>
+    <title>ログイン画面</title>
+</head>
+
+<body>
+    <script>
+        function maxLengthCheck(object) {
+            if (object.value.length > object.maxLength)
+                object.value = object.value.slice(0, object.maxLength)
+        }
+    </script>
+    <!--ヘッダー-->
+    <header>
+        <fieldset class="back2">
+            <center>
+                <h2>ログイン</h2>
+                <form action="./i_login.php" method="post">
+                    <p>ユーザーID：<input oninput="maxLengthCheck(this)" type="number" name="UserID" maxlength="12" required>
+                    </p>
+                    <p>パスワード：<input oninput="maxLengthCheck(this)" type="password" name="Password" maxlength="127" required></p>
+                    <p><input type="submit" name="login" value="ログイン" id="button2"></p>
+                </form>
+            </center>
+        </fieldset>
+</body>
+
+</html>
