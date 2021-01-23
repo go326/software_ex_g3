29d5195df964ea7f3cd97bb2ff0b164c3bf20cad
220299v <220299v@users.noreply.github.com>
2021-01-23 19:21:36 +0900

    1/21

---
+++ i_general_security.php
@@ -0,0 +1,8 @@
+<?php
+    function IFormSecurityP($form){
+        echo ($form."<br>");
+        $form = htmlspecialchars($form, ENT_QUOTES, "UTF-8");
+        echo ("変換後".$form."<br>");
+    }
+
+?>
